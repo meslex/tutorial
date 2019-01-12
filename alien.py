9564e3a82e1105bf9b037b59c52bac274850133e
meslex <ulimwegu@gmail.com>
2019-01-12 20:26:05 +0200

    Changed number of aliens

--- alien.py
+++ alien.py
@@ -1,15 +1,15 @@
 # Make an empty list for storing aliens.
 aliens = []
-bad_aliens = []
+
 
 # Make 30 green aliens.
 for alien_number in range (0,30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
-bad_aliens = aliens[0:3]
 
-for alien in bad_aliens:
+
+for alien in aliens:
     if alien['color'] == 'green':
         alien['color'] = 'yellow'
         alien['speed'] = 'medium'
